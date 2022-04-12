ec9f373015c8af03c3fcb4ca92bf1042ca24b385
최원영 <70950533+choewy@users.noreply.github.com>
2022-04-12 18:01:49 +0900

    rename to `./01-node-articles`

--- 01-node-react-blog/app/bin/www.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-require('dotenv').config();
-
-const app = require('../index');
-const port = process.env.NODE_PORT || 5000;
-const log = `Server Running on port ${port}`;
-
-app.listen(port, () => console.log(log));
