ede1634c89f97990fc71190b46ae4a82321bd5fe
choewy <choewy32@gmail.com>
2022-04-14 15:56:43 +0900

    app 구조 변경

--- 03-node-react-shop/server/bin/www.js
+++ 03-node-react-shop/server/bin/www.js
@@ -1,8 +1,5 @@
 'use strict';
 
-import app from '../app';
-
-// eslint-disable-next-line no-undef
-const port = process.env.PORT || 5000
-const log = `Server running on port ${port}`;
-app.listen(port, () => console.log(log));
+import App from '../app';
+const app = new App();
+app.listen();
